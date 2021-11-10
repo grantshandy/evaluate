b089c638801be4f221a19abb13a90697e3909958
grantshandy <grantshandy@gmail.com>
2021-11-09 18:54:09 -0700

    make a lil prettier

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import init, {eval_expression} from './wasm/wasm.js';
 
 const inputBox = document.getElementById("inputId");
-const submitButton = document.getElementById("submitId");
 const resultText = document.getElementById("result");
 
 (async () => {
   await init();
 
-  submitButton.onclick = function(){
+  inputBox.oninput = function() {
     resultText.innerHTML = eval_expression(inputBox.value);
-  };
+  }
 })();
